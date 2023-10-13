29bcb7bd8050bfc6b926efeb5716f220ba9cc517
wondwosen shiferaw <wondwosen.shi@gmail.com>
2023-10-13 18:04:26 +0200

    added useAxiosPrivate hooks and update refreshToken request.

--- src/components/post/AddItem.js
+++ src/components/post/AddItem.js
@@ -4,11 +4,12 @@ import { useTaskContext } from '../../context/TasksContextProvider'
 
 function AddItem( ) {
   const {addTask,handleChange,task} = useTaskContext()
-    const inputRef = useRef()
+  const inputRef = useRef()
+  const canSubmit = Boolean(task)
   return (
     <form onSubmit = {addTask} >
         <input autoFocus name="todo" type="text" ref={inputRef} placeholder = "Add item" value={task} onChange={(e) => handleChange(e)} />
-        <button type = "submit" onClick={()=> inputRef.current.focus()}>Add Task</button>
+        <button type = "submit" disabled = {!canSubmit} onClick={()=> inputRef.current.focus()}>Add Task</button>
     </form>
 )
 }
